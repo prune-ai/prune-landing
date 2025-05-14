1e743970195bb1f29c65f49c458ab031eb7ddd5a
KaranChouhan018 <jeemain145@gmail.com>
2025-05-14 12:32:33 +0530

    minor fixes

--- src/app/issue/page.tsx
+++ src/app/issue/page.tsx
@@ -1,36 +1,64 @@
 export default function Issues() {
   return (
     <div className="min-h-screen bg-[#18183B] text-white flex justify-center items-center flex-col py-16 px-4 text-start font-sans">
-        <div className="max-w-[570px] mx-auto py-[100px] text-white space-y-10">
+      <div className="max-w-[570px] mx-auto py-[100px] text-white space-y-10">
         <section>
-          <h2 className="text-xl  text-white font-mono mb-4">What’s the problem?</h2>
+          <h2 className="text-xl  text-white font-mono mb-4">
+            What’s the problem?
+          </h2>
           <p className="text-base font-inter leading-relaxed text-[#D4D4D8]">
-            The internet is a wild and blooming garden full of potential for creativity, connection and chaos. But beauty unchecked can grow over with ease. Every day, thousands of new links to non-consensual abuse material go live on the internet.
+            The internet is a wild and blooming garden full of potential for
+            creativity, connection and chaos. But beauty unchecked can grow over
+            with rot. Every day, thousands of new links to non-consensual abuse
+            material go live on the internet.
           </p>
           <p className="text-base font-inter leading-relaxed text-[#D4D4D8] mt-4">
-            Most of this content doesn’t live on mainstream platforms. 85-90% of persistent NCSAM circulation is hosted on the same: a fragmented network of unindexable abusive forums, turn a repost pipeline: fake tube clones, offshore hosts, and aggregators.
+            Most of this content doesn’t live on mainstream platforms. 85–90% of
+            persistent NCSAM circulates on shadow infrastructure: a fragmented
+            network of intentionally obscure foreign run pirated properties like
+            tube clones, offshore forums, and aggregators.
           </p>
           <p className="text-base font-inter  leading-relaxed text-[#D4D4D8] mt-4">
-            These sites are financially enabled by a complex web of ad networks, affiliate programs, and high-risk payment processors. Content is kept up for weeks, months, even years — often scabbed after takedown requests. There is often no clear standard for what action is required, or how quickly it should happen.
+            These sites are financially enabled by a complex web of ad networks,
+            affiliate programs, and high-risk payment processors. Content stays
+            up for weeks, months, years – even decades after takedown requests.
+            There are no enforceable standards for what action is required, or
+            how quickly it should happen.
           </p>
         </section>
 
         <section>
-          <h2 className="text-xl font-mono text-white mb-4">What we’re doing</h2>
+          <h2 className="text-xl font-mono text-white mb-4">
+            What we’re doing
+          </h2>
           <p className="text-base font-inter leading-relaxed text-[#D4D4D8]">
-            Pause is changing the way consent is enforced online. We are designing the infrastructure that makes consent enforceable, enforceability affordable, and at scale.
+            Prune is changing the way consent is enforced online. We are
+            designing the infrastructure that makes consent
+            enforceable—consistently, affordably, and at scale.
           </p>
           <p className="text-base  font-inter  leading-relaxed text-[#D4D4D8] mt-4">
-            First, we identify how harmful content is being more widely shared: through ad networks, content pipelines, affiliate schemes, and hosting infrastructure. Then we map the networks involved. If they’ll act, we escalate and partner with civil society.
+            First, we identify how harmful content is being monetized—whether
+            through ad networks, payment processors, affiliate schemes, or
+            hosting infrastructure. Then we notify the entities involved. If
+            they fail to act, we escalate and partner with victims to fight for
+            restitution.
           </p>
           <p className="text-base  font-inter  leading-relaxed text-[#D4D4D8] mt-4">
-            And since companies are so ready to clean up their systems, we offer free tools to do it: lightweight, legal, and partly computer vision-based tooling that supports platforms and payment providers in reducing non-consensual content at scale — without burdening moderation teams or creating watch trails.
+            And when companies are ready to clean up their systems, we offer the
+            tools to do it: lightweight, plug and play compliance tools like
+            user-facing takedown flows, systems that monitor and remove non
+            consensual content within minutes, and built-in documentation that
+            creates a clear audit trail.
           </p>
           <p className="text-base  font-inter  leading-relaxed text-[#D4D4D8] mt-4">
-            We don’t charge victims and we never will. Our work is funded by platforms that want to do it responsibly and by the legal consequences for those that don’t.
+            We don’t charge victims and we never will. Our work is funded by
+            platforms that want to act responsibly—and by the legal consequences
+            for those that don’t.
           </p>
           <p className="text-base  font-inter  leading-relaxed text-[#D4D4D8] mt-4">
-            We can’t control everything that grows online - but we can choose what to cut back. Let’s work together to create new standards for consent in the digital age.
+            We can’t control everything that grows online—but we can choose what
+            to cut back. Let’s work together to create new standards for consent
+            in the digital age.
           </p>
         </section>
       </div>
